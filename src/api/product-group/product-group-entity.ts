dfee83066a41961418ab74fd368dc475234958bc
Andre Lana <andre.padua.lana@gmail.com>
2021-08-26 19:13:45 -0300

    Start Product Group

--- src/api/product-group/product-group-entity.ts
+++ src/api/product-group/product-group-entity.ts
@@ -2,15 +2,7 @@ import { EntityBase } from "@api/base/entity-base";
 import { Column, Entity } from "typeorm";
 
 @Entity()
-export class Product extends EntityBase {
+export class ProductGroup extends EntityBase{
     @Column()
     name: string;
-
-    @Column()
-    description?: string;
-
-    @Column()
-    barCode?: string;
-   
-
 }
--- src/api/provider/provider-entity.ts
+++ src/api/provider/provider-entity.ts
@@ -1,5 +1,4 @@
 import { EntityBase } from "@api/base/entity-base";
 import { Entity } from "typeorm";
-
 @Entity()
 export class Provider extends EntityBase {}
