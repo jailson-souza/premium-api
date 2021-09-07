b2c0d7842c9ec971544841c2031b3e07debfd555
Andre Lana <andre.padua.lana@gmail.com>
2021-09-07 11:22:43 -0300

    update General Configuration

--- src/api/general-configuration/general-configuration-repository.ts
+++ src/api/general-configuration/general-configuration-repository.ts
@@ -1,7 +1,11 @@
 import { RepositoryBase } from "@api/base/repository-base";
 import { GeneralConfiguration } from "./general-configuration.entity";
+import { GeneralConfigurationRepositoryInterface } from "./interface/general-configuration-repository-interface";
 
-export class GeneralConfigurationrRepository extends RepositoryBase<GeneralConfiguration> implements GeneralConfigurationRepositoryInterface{
+export class GeneralConfigurationrRepository
+    extends RepositoryBase<GeneralConfiguration>
+    implements GeneralConfigurationRepositoryInterface
+{
     constructor(){
         super(GeneralConfiguration);
     }
