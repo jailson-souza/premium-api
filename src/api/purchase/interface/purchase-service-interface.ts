b035ba869b471c389fb770e7a637428ebaf0bc2a
Andre Lana <andre.padua.lana@gmail.com>
2021-09-09 15:33:46 -0300

    Purchase

---
+++ src/api/purchase/interface/purchase-service-interface.ts
@@ -0,0 +1,5 @@
+
+import { ServiceBase } from "@api/base/service-base";
+import { Purchase } from "../purchase-entity";
+
+export interface PurchaseServiceInterface extends ServiceBase<Purchase> {}
---
+++ src/api/purchase/purchase-controller.ts
@@ -0,0 +1,9 @@
+import { ControllerBase } from "@api/base/controller-base";
+import { Purchase } from "./purchase-entity";
+
+export class PurchaseController extends ControllerBase<Purchase> {
+    constructor({ purchaseService }){
+        super( purchaseService);
+    }
+}
+}
