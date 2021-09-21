cfc65d328b860ea699eb9490efd59ab06f533b89
Andre Lana <andre.padua.lana@gmail.com>
2021-09-21 14:04:43 -0300

    container

--- src/container.ts
+++ src/container.ts
@@ -81,7 +81,7 @@ const definition = {
     userGroupService: asClass(UserGroupService).singleton(),
     userRuleService: asClass(UserRuleService).singleton(),
     userService: asClass(UserService).singleton(),
-
+    
     // repository
     addressRepository: asClass(AddressRepository).singleton(),
     authRepository: asClass(AuthRepository).singleton(),
